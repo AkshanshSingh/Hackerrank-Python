83216b65016d1df593be0e30ca43e96087511c4f
Akshansh <43261823+AkshanshSingh@users.noreply.github.com>
2020-06-03 00:29:55 +0530

    Create Exceptions.py

---
+++ Exceptions.py
@@ -0,0 +1,9 @@
+T = int(input())
+for x in range(T):
+    a,b = input().split()
+    try:
+        print(int(a)//int(b))
+    except ZeroDivisionError as z:
+        print("Error Code:",z)
+    except ValueError as v:
+        print("Error Code:",v)
